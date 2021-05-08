ab1bc249110dc8b2d918d85fec263eedff1044de
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2021-05-08 18:32:25 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -52,3 +52,107 @@ $(document).ready(function () {
         }
     })
 });
+
+
+
+
+
+
+
+
+
+
+
+// Second Owl Script Here 
+$(document).ready(function () {
+    $('#SecondCarousel .owl-carousel').owlCarousel({
+        items: 5, // This Is The Number of Items You Want To show
+        margin: 20, // This Is The Gap Which You Want To Shows
+        loop: true, // Behaves As A Loop As The Default is False We Need To Change
+        nav: true, // It Will Show THe arrows 
+        autoplay: true, // Playing The Carousel Automatically
+        autoplayTimeout: 4000, // At What Time You Need The Carousel To scroll Automatically
+            dots : false,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:1,
+                },
+                600:{
+                    items:2,
+                },
+                1000:{
+                    items:2,
+                    dots : true
+                }
+            }
+        })
+});
+
+
+
+
+
+
+
+// Third Owl Script Here 
+$(document).ready(function () {
+    $('#ThirdCarousel .owl-carousel').owlCarousel({
+        items: 5, // This Is The Number of Items You Want To show
+        margin: 20, // This Is The Gap Which You Want To Shows
+        loop: true, // Behaves As A Loop As The Default is False We Need To Change
+        nav: true, // It Will Show THe arrows 
+        autoplay: true, // Playing The Carousel Automatically
+        autoplayTimeout: 5000, // At What Time You Need The Carousel To scroll Automatically
+        rtl: true,
+            dots : false,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:1,
+                },
+                600:{
+                    items:3,
+                },
+                1000:{
+                    items:4,
+                }
+            }
+        })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// Fourth Owl Script Here 
+$(document).ready(function () {
+    $('#FourthCarousel .owl-carousel').owlCarousel({
+        items: 5, // This Is The Number of Items You Want To show
+        margin: 20, // This Is The Gap Which You Want To Shows
+        loop: true, // Behaves As A Loop As The Default is False We Need To Change
+        nav: true, // It Will Show THe arrows 
+        autoplay: true, // Playing The Carousel Automatically
+        autoplayTimeout: 6000, // At What Time You Need The Carousel To scroll Automatically
+            dots : false,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:1,
+                },
+                600:{
+                    items:1,
+                },
+                1000:{
+                    items:3,
+                }
+            }
+        })
+});
