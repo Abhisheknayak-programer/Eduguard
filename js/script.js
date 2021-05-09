4e549d586d68b6f8c4ddb411d4c27e749ef77afd
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2021-05-09 09:10:40 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -147,8 +147,8 @@ $(document).ready(function () {
                 0:{
                     items:1,
                 },
-                600:{
-                    items:1,
+                650:{
+                    items:2,
                 },
                 1000:{
                     items:3,
